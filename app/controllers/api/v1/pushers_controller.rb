5e30f591194ca6f34a7c83fbd58094ac867cc591
吴捷胜 <cn.jason.wu@gmail.com>
2013-12-18 15:33:31 +0800

    finish and fixed the bug.add pusher module

--- app/controllers/api/v1/pushers_controller.rb
+++ app/controllers/api/v1/pushers_controller.rb
@@ -1,3 +1,4 @@
+#ending utf-8
 class Api::V1::PushersController < ApplicationController
 
   skip_before_filter :verify_authenticity_token
@@ -5,24 +6,21 @@ class Api::V1::PushersController < ApplicationController
   def push
     articleid = params[:id]
     project = Article.find_by(id: articleid).manager
-    push_module(project)
+    push_module(project,articleid)
   end
 
-
-
-
-  
-  def push_module(manager)
-    @users = Manager.find_by(id: manager.id).users
-    @users.each do |user|
+  def push_module(manager,articleid)
+    article = Article.find_by(id: articleid).content
+    users = Manager.find_by(id: manager.id).users
+    users.each do |user|
       token = new_token(manager, user)
       token = JSON.parse(token)
       access_token = token['access_token']
+      a = pusher(access_token,article)
       binding.pry
     end
+    render json: { message: "推送完成"}
   end
-
-
 
   def new_token(manager, user)
     RestClient.post "https://accounts.google.com/o/oauth2/token", {
@@ -31,6 +29,17 @@ class Api::V1::PushersController < ApplicationController
       refresh_token: user.refresh_token,
       grant_type: 'refresh_token'
       }
+  end
+
+  def pusher(access_token,article)
+    article = {"html" => article}
+    json = article.to_json
+    # binding.pry
+    RestClient.post 'https://www.googleapis.com/mirror/v1/timeline',json,{
+      Authorization: 'Bearer' + ' ' + access_token,
+      Content_Type: 'application/json'
+    }
+
   end
 
 
--- app/controllers/callback_controller.rb
+++ app/controllers/callback_controller.rb
@@ -1,12 +1,17 @@
+#ending utf-8
 class CallbackController < ApplicationController
   def authentication
     user_info = Manager.find_by(project_name: params[:project_name]).get_token(params[:code])
     user_info = JSON.parse( user_info )
-    a = Manager.find_by(project_name: params[:project_name]).users.create(uid: user_info['id_token'], token: user_info['access_token'], refresh_token: user_info['refresh_token'])
-    if a.save
-      @message = "sucess"
+    user = Manager.find_by(project_name: params[:project_name]).users.create(
+      uid: user_info['id_token'], 
+      token: user_info['access_token'], 
+      refresh_token: user_info['refresh_token']
+      )
+    if user.save
+      render json: {message: "授权成功"}
     else
-      @message = "fails"
+      render json: {message: "授权失败"}
     end
   end
 end
